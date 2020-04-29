a05dc3d876c2f6d7a16134e12b7b96ce3a3bc71f
tetiana.chernova <chtania.2000@gmail.com>
2020-04-29 21:05:54 +0300

    Initial commit

---
+++ src/main/java/com/chernova/folderstructure/FolderStructureApplication.java
@@ -0,0 +1,13 @@
+package com.chernova.folderstructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolderStructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FolderStructureApplication.class, args);
+	}
+
+}
