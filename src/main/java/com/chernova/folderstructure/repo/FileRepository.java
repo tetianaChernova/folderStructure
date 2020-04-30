147d4a43db344583e893a399b310306edb3c548e
tetiana.chernova <chtania.2000@gmail.com>
2020-04-30 13:15:49 +0300

    Created File and Folder repositories

---
+++ src/main/java/com/chernova/folderstructure/repo/FileRepository.java
@@ -0,0 +1,7 @@
+package com.chernova.folderstructure.repo;
+
+import com.chernova.folderstructure.model.File;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FileRepository extends CrudRepository<File, Long> {
+}
---
+++ src/main/java/com/chernova/folderstructure/repo/FolderRepository.java
@@ -0,0 +1,7 @@
+package com.chernova.folderstructure.repo;
+
+import com.chernova.folderstructure.model.Folder;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FolderRepository extends CrudRepository<Folder, Long> {
+}
